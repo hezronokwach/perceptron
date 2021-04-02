cdd7b5424694a5d43cd3224a30ee47b44e4855b1
Hezron Okwach <80953758+hezronokwach@users.noreply.github.com>
2021-04-02 03:51:25 +0300

    Add files via upload

---
+++ adaline.py
@@ -0,0 +1,51 @@
+
+
+
+import numpy as np
+import pandas as pd
+
+dataset = 'heart.data.txt'
+data=np.loadtext(dataset, delimiter=',')
+X = data.iloc[:, 0, 3].values
+y = data.iloc[:13].values
+
+
+class Adaline:
+    eta = 0.0
+    n_iter = 0
+    random_state = 0
+    w_ = np.zeros
+    cost = []
+
+    def __init__(self, eta=0.01, n_iter=50, random_state=1):
+        self.eta = eta
+        self.n_iter = n_iter
+        self.random_state = random_state
+
+    def fit(self, X, y):
+        rgen = np.random.RandomState(self.random_state)
+        self.w_ = rgen.normal(loc=0.0, scale=0.01, size=X.shape[1] + 1)
+        self.cost_ = []
+
+        for i in range(self.n_iter):
+            net_input = self.net_input(X)
+            output = self.activation(net_input)
+            errors = (y - output)
+            self.w_[1:] += self.eta * X.T.dot(errors)
+            self.w_[0] += self.eta * errors.sum()
+            cost = (errors ** 2).sum() / 2.0
+            self.cost_.append(cost)
+        return self
+
+    def net_input(self, X):
+        return np.dot(X, self.w_[1:] + self.w_[0])
+
+    def activation(self, X):
+        return X
+
+    def predict(self, X):
+        return np.where(self.activation(self.net_input(X)) >= 0.0, 1, -1)
+
+
+
+
